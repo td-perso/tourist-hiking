9d9e12d4fb99334804d52aa4b9c2bb231108c534
td-perso <thibaut.duby@outlook.fr>
2024-12-21 15:41:42 +0100

    fix: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,7 +29,7 @@ router.onError((err, to) => {
   }
 })
 
-router.addRoute({ path: '/:catchAll(.*)', redirect: { path: '/Index' } })
+router.addRoute({ path: '/:catchAll(.*)', redirect: { path: 'Index' } })
 
 router.isReady().then(() => {
   localStorage.removeItem('vuetify:dynamic-reload')
